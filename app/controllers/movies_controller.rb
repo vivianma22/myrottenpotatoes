e187c7d8421d4091e136974c43b9fb1af5dc34ee
Vivian <vma@uncc.edu>
2016-10-25 14:50:35 +0000

    Initialize repository

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -21,6 +21,17 @@ class MoviesController < ApplicationController
     redirect_to movies_path
   end
   
+  def edit 
+    @movie = Movie.find params[:id]
+  end
+  
+  def update
+    @movie = Movie.find params[:id]
+    @movie.update_attributes!(params[:movie])
+    flash[:notice] = "#{@movie.title} was successfully updated."
+    redirect_to movie_path(@movie)
+  end
+  
   private
   def user_params
    params.require(:movie)
